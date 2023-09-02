7bc32c118e03ad678d1548f48f2e71cd048597d9
BiKen <biken@okvip.com>
2023-09-02 13:45:52 +0700

    deploy

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,9 @@
 import Home from "~/pages/Home";
 
-const publicRoutes = [{ path: "/", component: Home }];
+const publicRoutes = [
+  { path: "/", component: Home },
+  { path: "/hi88-mb/", component: Home },
+];
 
 const privateRoutes = [];
 

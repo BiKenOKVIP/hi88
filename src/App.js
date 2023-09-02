8b3d22a2ec23bd23e49af1e9bb4739904e321d40
BiKen <biken@okvip.com>
2023-09-02 18:55:54 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { publicRoutes } from "~/routes";
 
 function App() {
   return (
-    <Router basename="/hi88-mb">
+    <Router basename="/hi88-mb/">
       <div style={{ display: "flex", height: "100%" }}>
         <Routes>
+          {/* Đặt trang mặc định */}
+          <Route path="/" element={<Navigate to="/hi88-mb/" />} />
           {publicRoutes.map((route, index) => {
             const Page = route.component;
             return <Route key={index} path={route.path} element={<Page />} />;
